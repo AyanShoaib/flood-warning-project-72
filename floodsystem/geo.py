7301d4056100ce33fe0f8cf2e401ef6179863b39
AyanShoaib <ayanshoaib2003@outlook.com>
2022-01-31 23:10:24 +0000

    Added Task1D fucntions below the comment of Task1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -11,18 +11,7 @@ from xmlrpc.server import SimpleXMLRPCRequestHandler
 from floodsystem.stationdata import build_station_list
 from .utils import sorted_by_key  # noqa
 
-
-def stations_by_rivers(stations):
-    stationsByRivername = {}
-
-    rivername = input("Enter River: ")
-    for station in stations:
-        if station.river in [ rivername ]:
-            if station.river in stationsByRivername:
-                stationsByRivername[station.river].append(station.name)
-            else:
-                stationsByRivername[station.river] = []
-    return stationsByRivername
+# Task 1D
 
 def rivers_with_stations(stations):
     riverswithstation = set()
@@ -31,3 +20,12 @@ def rivers_with_stations(stations):
             riverswithstation.add(station.river)
     return riverswithstation
 
+def stations_by_river(stations):
+    StationByRiver = {}
+    for station in stations:
+        if station.river in StationByRiver:
+            StationByRiver[station.river].append(station.name)
+            StationByRiver[station.river].sort()
+        else:
+            StationByRiver[station.river] = [station.name]
+    return StationByRiver
