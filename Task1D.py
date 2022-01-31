54c22d910b81076869a01df573dd4bda044a9529
AyanShoaib <ayanshoaib2003@outlook.com>
2022-01-31 23:09:48 +0000

    Finshed Task1D

--- Task1D.py
+++ Task1D.py
@@ -1,29 +1,38 @@
 from ast import Return
+from email import message
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import rivers_with_stations, stations_by_rivers
+from floodsystem.geo import rivers_with_stations
+from floodsystem.geo import stations_by_river
 
 
-def stationbyrivers_run():
-    """Requirements for Task 1A"""
-
-    # Build list of stations
-    stations = build_station_list()
-
-    sorted_stations = sorted(stations_by_rivers(stations))
-
-    print(sorted_stations)
 
 def riverswithstation_run():
 
     stations = build_station_list()
 
-    print("Stations: {}".format(len(rivers_with_stations(stations))))
+    print("There are {} rivers with stations, below are the first 10 in alphabetical arrangemnet".format(len(rivers_with_stations(stations))))
 
     sorted_rivers = sorted(rivers_with_stations(stations))
 
-    print(sorted_rivers)
+    print(sorted_rivers[:10])
+
+def stationsbyriver_run():
+
+    stations = build_station_list()
+
+    SBR = stations_by_river(stations)
+
+    River_Aire = SBR["River Aire"]
+    River_Thames = SBR["River Thames"]
+    River_Cam = SBR["River Cam"]
+
+    message = "The stations with allocated rivers are, River Aire: {} River Thames: {} River Cam: {}".format(River_Aire, River_Thames, River_Cam)
+
+    print(message)
+
 
 
 if __name__ == "__main__":
-   stationbyrivers_run()
    riverswithstation_run()
+   stationsbyriver_run()
+
