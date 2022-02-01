763b30afbd7e947ba7352a03e7e4ab3bc494b6db
AyanShoaib <ayanshoaib2003@outlook.com>
2022-02-01 00:18:32 +0000

    Coompleted Task 1E

---
+++ Task1E.py
@@ -0,0 +1,11 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+def RiverByStationNumber():
+
+    stations = build_station_list()
+
+    print(rivers_by_station_number(stations, 9))
+
+if __name__ == "__main__":
+    RiverByStationNumber()
