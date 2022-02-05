daad3b7c45507452619d9f813b35d7def247dcee
humzahsaleem <saleemhumzah@gmail.com>
2022-02-05 20:34:04 +0000

    Finished Function but error 'fucntion not iterable coming up'

--- Task1B.py
+++ Task1B.py
@@ -1,3 +1,4 @@
+from turtle import distance
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 
@@ -5,9 +6,11 @@ from floodsystem.stationdata import build_station_list
 def run():
     stations = build_station_list
     p = (52.1805492, 0.1323424)
-    test = stations_by_distance(stations, p)
+    distance = stations_by_distance(stations, p)
+
     
-    print(test)
+    print("Closest Stations: ", distance[:10])
+    print("Furthest Stations: ", distance[:-10])
 
 
 if __name__ == "__main__":
