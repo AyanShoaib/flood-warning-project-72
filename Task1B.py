969ed6ed209b3ad58cfc49bd16518a68451ad5fa
humzahsaleem <saleemhumzah@gmail.com>
2022-02-05 19:58:14 +0000

    Added Function

---
+++ Task1B.py
@@ -0,0 +1,14 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+
+def run():
+    stations = build_station_list
+    p = (52.1805492, 0.1323424)
+    test = stations_by_distance(stations, p)
+
+    print(test)
+
+
+if __name__ == "__main__":
+    run()
