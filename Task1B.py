1e3a3d4bddd3eae82471ad0523eef4951af3d83b
AyanShoaib <ayanshoaib2003@outlook.com>
2022-02-07 02:14:26 +0000

    Fixed code by adding () to line 7

--- Task1B.py
+++ Task1B.py
@@ -4,14 +4,15 @@ from floodsystem.stationdata import build_station_list
 
 
 def run():
-    stations = build_station_list
+    stations = build_station_list()
     p = (52.1805492, 0.1323424)
-    distance = stations_by_distance(stations, p)
+    d = stations_by_distance(stations, p)
 
     
-    print("Closest Stations: ", distance[:10])
-    print("Furthest Stations: ", distance[:-10])
+    print("The closest 10 stations are:", d[:10])
+    print ("The furthest 10 stations are:", d[-10:])
 
 
 if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
