fd3707610ab3afa1b8bb97384b023740e77c14a5
AyanShoaib <ayanshoaib2003@outlook.com>
2022-02-07 02:14:54 +0000

    added print statement to indicate what task it was

--- Task1C.py
+++ Task1C.py
@@ -3,9 +3,10 @@ from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 
 def run():
-    stations = build_station_list
+    stations = build_station_list()
     StationNearCambridge = stations_within_radius(stations, (52.2053,0.1218), 10)
-    print(StationNearCambridge)
+    print("Stations within a 10KM radius are: ", StationNearCambridge)
 
 if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
     run()
