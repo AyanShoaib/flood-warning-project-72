6618e1efd6f62b01301bd52f098fd940cf74a851
AyanShoaib <ayanshoaib2003@outlook.com>
2022-02-24 20:42:30 +0000

    Move fucntion to within the class, added to the repr

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -39,6 +39,7 @@ class MonitoringStation:
         d += "   town:          {}\n".format(self.town)
         d += "   river:         {}\n".format(self.river)
         d += "   typical range: {}".format(self.typical_range)
+        d += "   relative level:{}\n".format(self.relative_water_level())
         return d
     
     def typical_range_consistent(self):
@@ -50,7 +51,11 @@ class MonitoringStation:
         else:
             return False
         
-    
+    def relative_water_level(self):
+        if self.latest_level is None or not self.typical_range_consistent():
+            return None
+        return(self.latest_level - self.typical_range[0])/ (self.typical_range[1] - self.typical_range[0])
+
 
 
 def inconistant_typical_range_stations(stations):
@@ -61,21 +66,7 @@ def inconistant_typical_range_stations(stations):
     return inconsistant
 
 
-#Task 2B
-from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.utils import sorted_by_key
-
-def relative_water_level(self):
-    if (self.latest_level==None) or (not self.typical_range_consistent()):
-        return None
-    return(self.latest_level - self.typical_range[0])/ (self.typical_range[1 - self.typical_range[0]])
 
 
-def stations_level_over_threshold(stations, tol):
-    StationName = []
-    StationLevel = []
-    for station in stations:
-        if relative_water_level(station) > tol and relative_water_level(station) != None :
-            StationName.append(station.name)
-            StationLevel.append(station.latest_level)
-    return sorted_by_key(StationName, StationLevel)
+    
+
