9d440f3ca40cae9174a0adf09df00736d1a30403
AyanShoaib <ayanshoaib2003@outlook.com>
2022-02-24 20:41:45 +0000

    Just added a print and run fucntion

--- Task2B.py
+++ Task2B.py
@@ -1,9 +1,13 @@
-from floodsystem.station import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.utils import sorted_by_key
+
 
 def run():
 
     stations = build_station_list()
+    update_water_levels(stations)
+    print(stations_level_over_threshold(stations, 0.8))
 
-    return stations_level_over_threshold(stations, 0.8)
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
