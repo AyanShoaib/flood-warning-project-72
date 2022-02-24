abaff43efec45001c4430d101938957e009fd2e0
AyanShoaib <ayanshoaib2003@outlook.com>
2022-02-24 12:04:27 +0000

    added for Task2F

---
+++ floodsystem/analysis.py
@@ -0,0 +1,14 @@
+import matplotlib
+import numpy as np
+
+
+def polyfit(dates, levels, p):
+    x = matplotlib.dates.date2num(dates)
+    d0 = x[0]
+    x-= d0
+    y = levels
+
+    p_coeff = np.polyfit(x, y, 4)
+    poly = np.poly1d(p_coeff)
+
+    return poly, d0
