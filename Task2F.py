346aeae11306a8220c10579a73372283d8decc30
AyanShoaib <ayanshoaib2003@outlook.com>
2022-03-01 15:14:58 +0000

    added high water level from task 2c

--- Task2F.py
+++ Task2F.py
@@ -1,14 +1,17 @@
 import datetime
+from distutils.command.build import build
 from floodsystem.stationdata import update_water_levels, build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.plot import plot_water_level_with_fit
+from floodsystem.flood import stations_highest_rel_level
 
 def run():
     stations = build_station_list()
     update_water_levels(stations)
+    high_water_lev = stations_highest_rel_level(stations, 5)
 
     station_list = []
-    for station in stations:
+    for station in high_water_lev:
         station_list.append(station)
     
     for station in station_list:
