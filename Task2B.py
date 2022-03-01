b941eef19fcebca81c8589a1a782f8c00eec9af0
AyanShoaib <ayanshoaib2003@outlook.com>
2022-03-01 15:13:44 +0000

    added for loop to print the required stations

--- Task2B.py
+++ Task2B.py
@@ -6,7 +6,9 @@ def run():
 
     stations = build_station_list()
     update_water_levels(stations)
-    print(stations_level_over_threshold(stations, 0.8))
+    stations_level_over_threshold_list = stations_level_over_threshold(stations, 0.8)
+    for (station, level) in stations_level_over_threshold_list:
+        print(station.name," ",level)
 
 if __name__ == "__main__":
     print("*** Task 2B: CUED Part IA Flood Warning System ***")
